648ae89a302f451bb19b64965631dd753172b087
ianafeemuguiro <ianafeyyy@gmail.com>
2016-10-17 12:28:58 -0500

    Ejercicio Traducción Formulario

---
+++ traduccion-formulario/traduccion-formulario/js/main.js
@@ -0,0 +1,11 @@
+
+var form = document.getElementById ("form-signin-heading");
+form.innerHTML = "Acceder";
+document.getElementById ("inputEmail").setAttribute("placeholder","Correo Electronico");
+document.getElementById ("inputPassword") .setAttribute ("placeholder","Contraseña");
+document.getElementsByTagName ("span") [0] . innerHTML ="Recuérdame";
+document.getElementsByTagName ("button") [0] . innerHTML ="Iniciar Sección";
+
+
+
+ 

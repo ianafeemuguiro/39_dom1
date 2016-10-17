9f674af0afe0d499ebd1dc0c73436747191af9f3
ianafeemuguiro <ianafeyyy@gmail.com>
2016-10-17 16:59:23 -0500

    Formulario Bicis Locas

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,3 +1,22 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name").value;
+	var expreg = new RegExp("^[A-Z][A-Za-z]*$");
+	 if(expreg.test(nombre)){
+	    alert("Nombre Correcto");
+	    return false;
+	  	}else{
+	    	alert("Nombre Incorrecto");
+	    	return false;
+	}
+
+
+	var apellido = document.getElementById("lastname").value;
+	var expreg = new RegExp("^[A-Z][A-Za-z]*$");
+	 if(expreg.test(apellido)){
+	    alert("Apellido Correcto");
+	      return false;
+		}else{
+	    	alert("Apellido Incorrecto");
+	      	return false;
+	}
 }
